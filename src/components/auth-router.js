af0f9f3da2119c06bc8747b5180f18a5570c83a6
Michael Anokye <michaelanokyej@yahoo.com>
2020-03-03 20:10:18 -0500

    added auth endpoint

---
+++ src/components/auth-router.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const path = require("path");
+const xss = require("xss");
+const authRouter = express.Router();
+const bodyParser = express.json();
+const logger = require("../logger");
+const userService = require("./user-service");
+const { getAuthValidationError } = require("./auth-validator");
+const jwt = require("jsonwebtoken");
+
+const serializeauth = auth => ({
+  id: auth.id,
+  useremail: xss(auth.email),
+  firstName: xss(auth.first_name),
+  lastName: xss(auth.last_name),
+  userName: xss(auth.username)
+});
+
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader !== "undefined") {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
+
+authRouter
+  .route("/")
+
+  .post(bodyParser, (req, res, next) => {
+    const { userName } = req.body;
+    const user = { userName };
+
+    for (const field of ["userName"]) {
+      if (!user[field]) {
+        logger.error(`${field} is required`);
+        return res.status(400).send({
+          error: { message: `${field} is required` }
+        });
+      }
+    }
+
+    userService
+      .getByUsername(req.app.get("db"), user.userName)
+      .then(verifiedUser => {
+        res.json(serializeauth(verifiedUser));
+      })
+      .catch(next);
+  });
+
+authRouter
+  .route("/login")
+
+  .post(bodyParser, (req, res, next) => {
+    const { email, password } = req.body;
+    const user = { email, password };
+
+    for (const field of ["email", "password"]) {
+      if (!user[field]) {
+        logger.error(`${field} is required`);
+        return res.status(400).send({
+          error: { message: `${field} is required` }
+        });
+      }
+    }
+
+    const error = getAuthValidationError(user);
+
+    // if (error) return res.status(400).send(error);
+    if (error) return res.status(400);
+
+
+    userService
+      .getByUserEmail(req.app.get("db"), user.email)
+      .then(verifiedUser => {
+        if (verifiedUser.password !== user.password) {
+          res.send("Err: User not found, please verify password");
+        } else {
+          return verifiedUser;
+        }
+      })
+      .then(user => {
+        jwt.sign({ user }, "secretkey", (err, token) => {
+          res.json({
+            token,
+            ...user
+          });
+        });
+      })
+      .catch(next);
+  });
+
+
+module.exports = authRouter;
